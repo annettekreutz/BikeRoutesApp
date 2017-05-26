4b2a2461bcba7cd95c7f69adb34d0aa58439d4d0
Team iOS <team_ios@Poolgerats-MacBook-Air.local>
2017-05-26 17:01:59 +0200

    Initial Commit

---
+++ BikeHistorie/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  BikeHistorie
+//
+//  Created by Team_iOS on 26.05.17.
+//  Copyright © 2017 Team_iOS. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
